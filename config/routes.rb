d48bf853477e3d17e3d987aad3706ad52114e05f
justin <jayman1294@gmail.com>
2020-04-13 01:34:35 -0400

    generated session controller with new create destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get '/signup', to: 'users#new'
-  
+  get 'login', to: 'users#show'
 
   resources :orders
   resources :users
