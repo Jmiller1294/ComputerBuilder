53d5f866815d46e07ebbf07704168e99e35d3d50
justin <jayman1294@gmail.com>
2020-04-14 01:38:14 -0400

    added sessions create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,16 @@ class SessionsController < ApplicationController
   
   
   def new
-    
   end
 
   def create
-
+    @user = User.find_by(username: params[:username])
+    session[:user_id] = params[:user_id]
+    redirect_to user_path(@user)
   end
 
   def destroy
+    session.delete :username
+    redirect_to "/"
   end
 end
